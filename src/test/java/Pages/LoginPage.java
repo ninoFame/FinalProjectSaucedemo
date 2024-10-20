59fb250f0183c89e6afb7fccfcfc7e207cf626f8
ninoFame <niniappmail@gmail.com>
2024-10-21 00:07:58 +0200

    Basic Login tests added

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,16 +1,21 @@
 package Pages;
 
 import Base.BaseTest;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomepagePage extends BaseTest {
+
     public HomepagePage() {
         PageFactory.initElements(driver, this);
     }
 
-    //Username Field
+    @FindBy (className = "login_logo")
+    public WebElement swagLabs;
+
     @FindBy (id = "user-name")
     public WebElement usernameField;
 
@@ -23,5 +28,11 @@ public class HomepagePage extends BaseTest {
     public void clickOnLoginButton () {
         loginButton.click();
     }
+
+    @FindBy (css = ".login_credentials_wrap-inner")
+    public WebElement logginCredentials;
+
+    @FindBy (css = "h3[data-test='error']")
+    public WebElement errorMessage;
 
 }
